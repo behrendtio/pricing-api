90580748e5e0d84912917c4525cc51ddc2141e76
Ahter Sönmez <ahter3@gmail.com>
2018-07-25 15:48:33 +0100

    draft test with pytest

--- pricing_core/tests.py
+++ pricing_core/tests.py
@@ -1,16 +1,30 @@
-from django.test import TestCase
+import json
+import pytest
 
-from unittest import skip
+from pricing_core.models import Order
 
-from django.contrib.auth.models import User
-# from django.urls import reverse
 
-# from pricing_core.models import Order, Product
+@pytest.mark.django_db(transaction=False)
+def test_the_endpoint_creates_order(client, django_user_model):
+    assert Order.objects.exists() is False
+    client, _ = get_logged_in_client(client, django_user_model)
+    headers = {'content-type': 'application/json'}
+    payload = {'customer': 3, 'items': [{'id': 3, 'quantity': 1}, {'id': 4, 'quantity': 2}]}
 
-user = User.object.get(id=1)
+    response = client.post("http://127.0.0.1:9000/api/orders/", data=json.dumps(payload), headers=headers)
 
-from rest_framework.test import APIRequestFactory
+    assert response.status_code == 201
+    response_json = response.json()
+    for item in payload.items():
+        assert response_json[item] == item
 
-# Using the standard RequestFactory API to create a form POST request
-factory = APIRequestFactory()
-request = factory.post('/', {'user': user})
+    assert Order.objects.filter(id=response_json["id"]).exists()
+    assert Order.objects.all().count() == 1
+
+
+def get_logged_in_client(client, django_user_model):
+    username = "user1"
+    password = "pass1"
+    user = django_user_model.objects.create_user(username=username, password=password)
+    client.login(username=username, password=password)
+    return client, user
