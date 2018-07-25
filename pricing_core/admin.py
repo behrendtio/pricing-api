cf6492c6f72bb29a398d63e3702c3c0ee4dd47c8
Ahter Sönmez <ahter3@gmail.com>
2018-07-25 15:33:52 +0100

    adding the item quantity

--- pricing_core/admin.py
+++ pricing_core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from pricing_core.models import Product, Order
+from pricing_core.models import Product, Order, ItemQuantity
 
 
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(ItemQuantity)
