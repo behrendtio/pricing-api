50d805eca81afb9f1d73b66d4ccaae11daeac9ab
Ahter Sönmez <ahter3@gmail.com>
2018-07-25 23:43:35 +0100

    creating products and fixing header errors

--- pricing_core/tests.py
+++ pricing_core/tests.py
@@ -1,8 +1,11 @@
 import json
 import pytest
+from django.contrib.auth.models import User
+
 from rest_framework.test import RequestsClient
 
-from pricing_core.models import Order
+from pricing_core.models import Order, Product
+
 
 @pytest.mark.django_db(transaction=False)
 def test_requests():
@@ -13,27 +16,45 @@ def test_requests():
 
 @pytest.mark.django_db(transaction=False)
 def test_the_endpoint_creates_order(client, django_user_model):
+
+    # Creating the products that will be used in the tests
+    Product.objects.create(name="Book", price=599, vat_band="st")
+    Product.objects.create(name="White Bread", price=250, vat_band="ze")
+    Product.objects.create(name="Wholegrain Bread", price=250, vat_band="ze")
+    Product.objects.create(name="Computer", price=1000, vat_band="ze")
+    Product.objects.create(name="Car", price=1250, vat_band="st")
+
+    # Creating the customer
+    user = User.objects.create(username="user", email='test@test.com', is_active=True)
+    user.set_password('test')
+    user.save()
+
+    # Testing
     assert Order.objects.exists() is False
-    client = RequestsClient()
+    #client = RequestsClient()
 
     #client, _ = get_logged_in_client(client, django_user_model)
-    headers = {'content-type': 'application/json'}
-    payload = {'customer': 3, 'items': [{'id': 3, 'quantity': 1}, {'id': 4, 'quantity': 2}]}
+    payload = {"customer": 1, "items": [{"id": 3, "quantity": 1}, {"id": 4, "quantity": 2}]}
 
-    response = client.post("http://testserverapi/api/orders/", data=json.dumps(payload), headers=headers)
+    response = client.post("http://testserverapi/api/orders/", data=json.dumps(payload), content_type="application/json")
 
     assert response.status_code == 201
     response_json = response.json()
-    for item in payload.items():
-        assert response_json[item] == item
 
+    # checking if the order was created
     assert Order.objects.filter(id=response_json["id"]).exists()
     assert Order.objects.all().count() == 1
 
+    # checking if the order belongs to the right customer
+    assert response_json["customer"] == payload["customer"]
 
-def get_logged_in_client(client, django_user_model):
-    username = "user1"
-    password = "pass1"
-    user = django_user_model.objects.create_user(username=username, password=password)
-    client.login(username=username, password=password)
-    return client, user
+    # checking if the same number of items were added to the order
+    assert len(response_json["items"]) == len(payload["items"])
+
+
+# def get_logged_in_client(client, django_user_model):
+#     username = "user1"
+#     password = "pass1"
+#     user = django_user_model.objects.create_user(username=username, password=password)
+#     client.login(username=username, password=password)
+#     return client, user
