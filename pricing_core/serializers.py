d3ce39720f5d2fc11e7c261cc401dfffc533cf86
Ahter Sönmez <ahter3@gmail.com>
2018-07-25 18:18:54 +0100

    adding the item quantity serializer

--- pricing_core/serializers.py
+++ pricing_core/serializers.py
@@ -9,9 +9,23 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ('id', 'price', 'vat')
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    items = ProductSerializer(many=True, read_only=True)
+class ItemQuantitySerializer(serializers.ModelSerializer):
+    order = serializers.ReadOnlyField(source='*')
+    product = serializers.ReadOnlyField(source='*')  # passing all
+    #order = OrderSerializer(read_only=True)
+    #product = ProductSerializer(read_only=True)
 
     class Meta:
-        fields = ('id', 'customer', 'items', 'order_total', 'vat_total')
+        model = models.ItemQuantity
+        fields = ('quantity')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = ProductSerializer(many=True, read_only=True)
+    quantity = ItemQuantitySerializer(source='*', read_only=True)
+
+    #currency = (allow_null = True)
+
+    class Meta:
+        fields = ('id', 'customer', 'items', 'quantity', 'order_total', 'vat_total')
         model = models.Order
